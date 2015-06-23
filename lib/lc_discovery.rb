e43c6f643bd6014e38593e67c0f407be95e23faf
R. Bryan Hughes <rbhughes@logicalcat.com>
2015-06-23 11:57:15 -0700

    redis initialization

--- lib/lc_discovery.rb
+++ lib/lc_discovery.rb
@@ -1,7 +1,7 @@
 require 'lc_discovery/version'
 require 'lc_discovery/discovery'
 require 'lc_discovery/extracts'
-require 'lc_discovery/stats'
+#require 'lc_discovery/stats'
 require 'lc_discovery/sybase'
 require 'lc_discovery/utility'
 #require 'sidekiq'
